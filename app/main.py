9145234b7f032f0b2acbb39831ebbb57262ac3c7
lrrmarker <laurents.marker@ncas.ac.uk>
2025-06-19 10:08:29 +0100

    cleaning up for remote

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Path, HTTPException
+from typing import Annotated
 import json
 import datetime as dt
 from bokeh.plotting import figure
@@ -21,6 +22,14 @@ curdoc().theme = 'dark_minimal'
 @app.get('/')
 def root():
     return True
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+@app.get("/items/")
+async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
+
 
 @app.post('/plot')
 def get_plot(args: TimeseriesArgs):
@@ -33,3 +42,15 @@ def get_plot(args: TimeseriesArgs):
     p.yaxis.axis_label = y_axis_label
     j = json_item(p, 'bk-plot', theme='dark_minimal')
     return j
+
+
+
+# Dependency function that takes item_id as input
+def validate_item_id(item_id: int = Path(..., gt=0)):
+    if item_id == 13:
+        raise HTTPException(status_code=400, detail="Item 13 is forbidden")
+    return item_id
+
+@app.get("/items/{item_id_0}/{item_id_1}")
+def read_item(validated_id_0: int = Depends(validate_item_id), validated_id_1: int = Depends(validate_item_id)):
+    return {"item_id": validated_id}
